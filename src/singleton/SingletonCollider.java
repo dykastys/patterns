41633fd2cd4700e04ad8535f0058980375664357
dykastys <kirill.danke@mail.ru>
2023-06-04 12:11:16 +0700

    singleton

---
+++ src/singleton/SingletonCollider.java
@@ -0,0 +1,11 @@
+package singleton;
+
+public class SingletonCollider {
+
+    public static void main(String[] args) {
+        Singleton singleton1 = Singleton.getInstance();
+        Singleton singleton2 = Singleton.getInstance();
+
+        System.out.println(singleton1 == singleton2);
+    }
+}
